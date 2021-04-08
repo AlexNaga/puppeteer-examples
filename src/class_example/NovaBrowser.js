f450b22b1f3cd100c7fd2589d5ed551929d126fd
Alex <alex.naga@postnord.com>
2021-04-08 06:59:28 +0200

    chore: update class

--- src/class_example/NovaBrowser.js
+++ src/class_example/NovaBrowser.js
@@ -2,13 +2,15 @@ const { Browser } = require("./Browser");
 
 class NovaBrowser extends Browser {
   constructor({ url, headless }) {
-    // Here, it calls the parent class' constructor with url
+    // Call the parent class' constructor with options
     super({ url, headless });
-    // Note: In derived classes, super() must be called before you
-    // can use 'this'. Leaving this out will cause a reference error.
+    /**
+     * Note: In derived classes, super() must be called before you
+     * can use 'this'. Leaving this out will cause a reference error.
+     */
   }
 
-  login() {
+  async login() {
     console.log("Login to Nova");
   }
 }
