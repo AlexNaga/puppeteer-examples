5b894d35ee468e2d04cb7027bd0c3d4ddf116e43
Alex <alex.naga@postnord.com>
2021-04-11 11:29:30 +0200

    feat: add example of exposing a function to the page

--- src/examples/example_5.js
+++ src/examples/example_5.js
@@ -1,20 +1,20 @@
 const puppeteer = require("puppeteer");
 
-// Example of getting the page title
+// Example of adding our own function to the page
 (async () => {
   try {
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
-    await page.goto("https://sj.se");
-
-    const pageTitle = await page.title();
-    console.log(`The page title is: ${pageTitle}`);
 
     page.on("console", (msg) => console.log(msg.text()));
 
+    // Add our own function to the page
+    await page.exposeFunction("myFunction", (txt) => txt + " (Hello from myFunction)");
+
     // Function that is run within the page
     await page.evaluate(async () => {
-      console.log(document.title);
+      const result = await window.myFunction("This is a text");
+      console.log(result);
     });
 
     await browser.close();
